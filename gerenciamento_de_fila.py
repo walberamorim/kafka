2b382b3db378caf848dc0b35adc5347092e8f165
Walber Amorim <walber_amorim@hotmail.com>
2025-04-11 20:30:31 -0300

    0.0.1

--- gerenciamento_de_fila.py
+++ gerenciamento_de_fila.py
@@ -4,6 +4,7 @@ import random
 import json
 
 ESPECIALIDADES = ['ortopedista', 'cardiologiasta', 'ginecologista', 'pediatra', 'neurologista']
+TOTAL_PACIENTES = 20
 
 fake = Faker('pt_BR')
 
@@ -20,8 +21,8 @@ def iniciar_fila_pacientes():
     iniciada = False
     try:
         cliente = KafkaClient(
-            bootstrap_servers='0.0.0.0:9092',
-            api_version=(0, 11, 5))
+            bootstrap_servers=['localhost:9092'],
+            api_version=(0, 10, 1))
         cliente.add_topic('pacientes'),
         cliente.close(),
         iniciada = True
@@ -29,6 +30,25 @@ def iniciar_fila_pacientes():
         print(f"Erro ao conectar ao Kafka: {e}")
     return iniciada
 
+def on_sucesso(mensagem):
+    print(f'Sucesso ao enviar paciente: {mensagem}')
+
+def on_erro(mensagem):
+    print(f'Erro ao enviar paciente: {mensagem}')
+
+def gerar_fila_atendimento():
+        produtor = KafkaProducer(
+            bootstrap_servers=['localhost:9092'],
+            api_version=(0, 10, 1))
+        for i in range(TOTAL_PACIENTES):
+            paciente = gerar_paciente_fake()
+            print(f"Paciente gerado: " + paciente['nome'] + " - " + paciente['especialidade'])
+            envio = produtor.send(topic='pacientes', value=json.dumps(paciente).encode('utf-8'))
+            envio.add_callback(on_sucesso).add_errback(on_erro)
+            produtor.flush()
+        produtor.close()
+
 if __name__ == '__main__':
-    paciente = gerar_paciente_fake()
-    print(paciente)
+    iniciada = iniciar_fila_pacientes()
+    if iniciada:
+        gerar_fila_atendimento()
